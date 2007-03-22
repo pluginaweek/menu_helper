032c0d1a3194bdbeb355312f08fb4e1adb320adb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-03-22 14:45:09 +0000

    Initial revision.

---
+++ lib/menu_helper.rb
@@ -0,0 +1,64 @@
+require 'set_or_append'
+
+module PluginAWeek #:nodoc:
+  module Helpers #:nodoc:
+    # 
+    module MenuHelper
+      # 
+      def menu_bar(options = {}, html_options = {}, &block)
+        MenuBar.new(options, html_options, &block)
+      end
+      
+      # 
+      class Menu
+        include ActionView::Helpers::TagHelper
+        
+        # The collection of options to use in the cell's html
+        attr_reader :html_options
+        
+        delegate    :[],
+                    :[]=,
+                      :to => :html_options
+        
+        def initialize(tag_name, class_name, content = class_name.to_s.titleize, html_options = {}) #:nodoc
+          @html_options = html_options.symbolize_keys
+          @html_options.set_or_prepend(:class, class_name.to_s)
+          
+          @tag_name, @content = tag_name, content
+        end
+        
+        # 
+        def build
+          content_tag(@tag_name, @content, @html_options)
+        end
+      end
+      
+      #   
+      class MenuBar
+        include ActionView::Helpers::TagHelper
+        
+        # 
+        attr_reader :menus
+        
+        def initialize(collection, options = {}, html_options = {}) #:nodoc:
+          
+          yield self if block_given?
+        end
+        
+        # 
+        def menu(class_name, caption = class_name.to_s.titleize)
+          @menus[class_name] = caption
+        end
+        
+        # 
+        def build
+          html = ''
+        end
+      end
+    end
+  end
+end
+
+ActionController::Base.class_eval do
+  helper PluginAWeek::Helpers::MenuHelper
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+require 'rubygems'
+require 'action_controller'
+require 'action_view'
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require File.dirname(__FILE__) + '/../init'
